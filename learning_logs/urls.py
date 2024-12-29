8000c426fb842285f3289f10bfc139cfb7418288
misael <21170010a@gmail.com>
2024-12-29 09:45:06 -0600

    se empiezan a crear los formularios

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
     path('topics/', views.topics,name='topics'),
     #Página de detalles sobre un tema individual.
     path('topics/<int:topic_id>/',views.topic,name='topic'),
+    #Página para añadir un nuevo tema.
+    path('new_topic/', views.new_topic, name='new_topic'),
+    #Página para añadir una entrada nueva.
+    path('new_entry/<int:topic_id>/',views.new_entry, name='new_entry')
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Topic
+from .forms import TopicForm, EntryForm
 
 def index(request):
     '''Pagina de inicio para Learning Log.'''
@@ -17,3 +18,37 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic':topic,'entries':entries}
     return render(request,'learning_logs/topic.html',context)
+
+def new_topic(request):
+    '''Añade un nuevo tema.'''
+    if request.method != 'POST':
+        #No se han enviado los datos; crea un formulario en blanco.
+        form = TopicForm()
+    else:
+        #Datos POST enviados; procesa datos.
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+
+    #Muestra el formulario en blanco o no válido.
+    context = {'form':form}
+    return render(request,'learning_logs/new_topic.html',context)
+
+
+def new_entry(request,topic_id):
+    '''Añade una entrada nueva para un tema en particular.'''
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+
+    context = {'topic':topic,'form':form}
+    return render(request, 'learning_logs/new_entry.html',context)
